46590e1c5fc29d4b9e05c786b5994567bb2d9121
koksoybedirhan <71734713+koksoybedirhan@users.noreply.github.com>
2022-07-08 02:04:32 +0300

    Update LoRa-Transmitter.ino

--- LoRa-Communication/LoRa-Transmitter/LoRa-Transmitter.ino
+++ LoRa-Communication/LoRa-Transmitter/LoRa-Transmitter.ino
@@ -3,31 +3,25 @@
 #include "TinyGPS++.h"
 #include <SoftwareSerial.h>
 
-SoftwareSerial portLora(19, 18);
+SoftwareSerial portLora(10, 11);
 LoRa_E32 e32ttl(&portLora);
 
 TinyGPSPlus gps;
 
-float hizfloat = 34;
-float latfloat = 41.342341;
-float longfloat = 28.412512;
-float altfloat = 81.5;
-float voltfloat = 75;
-float enerjifloat = 46;
-float sicaklik1float = 29;
-float sicaklik2float = 50;
-float sicaklik3float = 13;
+float sicaklik1float = 36.0;
+float hizfloat = 52.0;
+float voltfloat = 5182.0;
+float enerjifloat = 54.0;
+float sicaklik2float = 70.0;
+float sicaklik3float = 13.0;
 
 typedef struct {
-byte altitude[5];
-byte latitude[10];
-byte longtitude[10];
-byte hiz[3];
-byte volt[3];
-byte enerji[3];
-byte sicaklik1[3];
-byte sicaklik2[3];
-byte sicaklik3[3];
+byte sicaklik1[5];
+byte hiz[5];
+byte volt[5];
+byte enerji[5];
+byte sicaklik2[5];
+byte sicaklik3[5];
 } Signal;
 Signal data;
 
@@ -41,28 +35,15 @@ void setup() {
 
 void loop() 
 {
- // while(Serial2.available())
-  //{
-    *(float*)(data.altitude) = altfloat;
-    *(float*)(data.latitude) = latfloat;
-    *(float*)(data.longtitude) = longfloat;
-    *(float*)(data.hiz) = hizfloat;
-    *(float*)(data.volt) = voltfloat;
-    *(float*)(data.enerji) = enerjifloat;
-    *(float*)(data.sicaklik1) = sicaklik1float;
-    *(float*)(data.sicaklik2) = sicaklik2float;
-    *(float*)(data.sicaklik3) = sicaklik3float;
-
-    Serial.println(*(float*)data.altitude);
-    Serial.println(*(float*)data.latitude);
-    Serial.println(*(float*)data.longtitude);
-    Serial.println(*(float*)data.hiz);
-    Serial.println(*(float*)data.volt);
-    Serial.println(*(float*)data.enerji);
-    Serial.println(*(float*)data.sicaklik1);
-    Serial.println(*(float*)data.sicaklik2);
-    Serial.println(*(float*)data.sicaklik3);
-    /*if(gps.encode(Serial2.read()))
+  *(float*)(data.sicaklik1) = sicaklik1float;
+  *(float*)(data.hiz) = hizfloat;
+  *(float*)(data.volt) = voltfloat;
+  *(float*)(data.enerji) = enerjifloat;
+  *(float*)(data.sicaklik2) = sicaklik2float;
+  *(float*)(data.sicaklik3) = sicaklik3float;
+  /*while(Serial2.available())
+  {
+    if(gps.encode(Serial2.read()))
     {
       String msg = Serial2.readStringUntil('\r');
       Serial.println(msg);
@@ -74,10 +55,9 @@ void loop()
       Serial.print("ALT="); Serial.println(gps.altitude.meters(), 1);
       (float)(data.altitude) = (gps.altitude.meters());
       delay(4*1000);*/
-
-      ResponseStatus rs = e32ttl.sendFixedMessage(0, 7, 17, &data, sizeof(Signal));
-      Serial.println(rs.getResponseDescription());
-      delay(650);
     //}      
   //}  
+  ResponseStatus rs = e32ttl.sendFixedMessage(0, 16, 29, &data, sizeof(Signal));
+  Serial.println(rs.getResponseDescription());
+  delay(650);
 }
